fd3b038627b59414645f25277dd788c4823962f6
Abdullah Shoab <imshoab883@gmail.com>
2024-12-30 14:41:36 +0600

    Add files via upload

---
+++ userInput.py
@@ -0,0 +1,7 @@
+name = input("Enter your name:")
+age = int(input("Enter your age:"))
+marks = float(input("Enter your marks:"))
+
+print("Welcome", name)
+print("Your age is:", age)
+print("Your mark is:", marks)
