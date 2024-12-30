42c0a7dedf50051dd23b69f59da2ad7215f12abd
Abdullah Shoab <imshoab883@gmail.com>
2024-12-30 10:26:21 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,18 @@
+myName = "Shoab"
+myAge = 23
+myHight = 5.10
+relationShipStatus = False
+
+print("My name is:", myName)
+print(type(myName))
+print("\n")
+print("My age is:", myAge)
+print(type(myAge))
+print("\n")
+print("My hight is:", myHight)
+print(type(myHight))
+print("\n")
+print("My relation ship Status is",relationShipStatus)
+print(type(relationShipStatus))
+print("\n")
+
